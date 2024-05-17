9f2bbf6a462878c42c0741a5459eedce24b1cf37
Darshan-13197 <darshanvd13197@gmail.com>
2024-05-17 23:24:02 +0530

    roboshop script

--- roboshop.sh
+++ roboshop.sh
@@ -3,7 +3,7 @@
 #replace the AMI, SG_ID, ZONE_ID, Domain
 AMI=ami-0f3c7d07486cad139
 SG_ID=sg-0dfadc3db09f8f96d
-ZONE_ID=
+ZONE_ID=Z01824442MLFHOL5P289V
 DOMAIN_NAME="darshanshop.online"
 
 # Creating as an Array
@@ -24,6 +24,26 @@ do
     IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-0dfadc3db09f8f96d --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text) #Instances[0] like an array
 
     echo "$i: $IP_ADDRESS" #will get to know the IP address of the Instances
+
+    #create R53 record, make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A" 
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
+        }]
+    }
+    '
 
 done
 
